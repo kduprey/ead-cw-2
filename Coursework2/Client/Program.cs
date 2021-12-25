5c9df12136a982201d4e7ca04408d34e12171821
Kenton Duprey <kentonduprey@icloud.com>
2021-12-24 17:34:50 -0700

    first commit starter template

---
+++ Coursework2/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Coursework2.Client;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Syncfusion.Blazor;
+
+
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NTU0MDA2QDMxMzkyZTM0MmUzMGowMmJRb0hsbnpERHE0ZTZ0eDI2OStVdllFRDRZWUFCalFLTGNvWHd5eU09");
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSyncfusionBlazor();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
